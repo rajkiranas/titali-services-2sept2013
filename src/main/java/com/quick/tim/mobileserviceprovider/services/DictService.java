aa92a6b16714f060b2870bb2f434e8e916d7896d
rajkiranas@gmail.com <rajkiranas@gmail.com@1191e5e1-4211-8d10-49b4-d7e1fbf50356>
2013-11-15 12:48:30 +0000

    dictionary - add word

--- src/main/java/com/quick/tim/mobileserviceprovider/services/DictService.java
+++ src/main/java/com/quick/tim/mobileserviceprovider/services/DictService.java
@@ -6,6 +6,10 @@ package com.quick.tim.mobileserviceprovider.services;
 
 import com.quick.tim.mobileserviceprovider.DAO.DictDao;
 import com.quick.tim.mobileserviceprovider.bean.DictWordDetailsBean;
+import com.quick.tim.mobileserviceprovider.entity.DictList;
+import com.quick.tim.mobileserviceprovider.entity.ForumEventDetails;
+import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
+import java.util.Date;
 import java.util.List;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
@@ -28,4 +32,17 @@ public class DictService
            
            return list;
        }
+       
+    public void saveNewWordDetails(JSONObject inputRequest) throws Exception {
+        DictList word = new DictList();
+        
+        word.setWord(inputRequest.getString("word"));
+        word.setMeaning(inputRequest.getString("meaning"));
+        word.setLabels(inputRequest.getString("labels"));
+        word.setAddDate(new Date());
+        word.setOwnerUsername(inputRequest.getString("owner_username"));
+        word.setOwnerName(inputRequest.getString("owner_name"));
+
+        dictDao.saveNewWordDetails(word);
+    }
 }
